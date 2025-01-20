2e01ce2bcecafa2b35e93fd453e1a74c088aa520
Polina Shatueva414 <shatuevapolina@icloud.com>
2025-01-20 18:52:57 +0700

    done

--- task1.py
+++ task1.py
@@ -7,4 +7,15 @@
 with open ('task1.txt', encoding="utf-8") as f:
     file_line=f.readlines()
     print(len (file_line))
+    a = 0
+    c = 0
+    for line in file_line:
+        b = line.split()
+        c = c + len(b)
+    print(c)
+    for line in file_line:
+        d = len(line)
+    print(d)
+
+
     
--- task5.py
+++ task5.py
@@ -5,3 +5,24 @@
 # Его длина: длина
 #
 # Выведите это слово и длину в консоль.
+m = 0
+with open("task5.txt","r", encoding="utf-8") as f:
+    a = f.read()
+    print(a)
+    b = a.replace("\n", "")
+    print(b)
+    b = b.replace(".", "")
+    print(b)
+    b = b.replace("!","")
+    print(b)
+    c = b.split()
+    print(c)
+    c.sort(key = len)
+    print(c)
+    i = max(c, key=len)
+    i1=len(i)
+    print("Max word:", max(c, key=len))
+    print(len(i))
+    with open("task5_2", "w", encoding="utf-8") as f2:
+        f2.write(i + "-Max word")
+        f2.write(f' Its lenth: {len(i)}')
